84b973746f12a1377739577bbb1f03c04d7804c8
juano2123 <juan_man.perlaza@uao.edu.co>
2023-02-27 17:57:53 -0500

    inicio de sesion

---
+++ src/pages/Inicio_sesion.jsx
@@ -0,0 +1,18 @@
+import logo_romo_blanco from '../images/logo_romo_blanco.png'
+import './styles/Inicio_sesion.css';
+function Inicio_sesion() {
+  return (
+    <div className="Inicio_sesion">
+      <header>
+        <img className='logo' src={logo_romo_blanco}></img>
+        <button>hola</button>
+        <tag> 
+        <h2>hols</h2>
+        </tag>
+
+      </header>
+    </div>
+  );
+}
+
+export default Inicio_sesion;
