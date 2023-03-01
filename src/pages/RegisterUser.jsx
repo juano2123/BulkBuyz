1227d9f2e4b1881149d7c165b5fe7c9241a245ea
juano2123 <juan_man.perlaza@uao.edu.co>
2023-03-01 02:31:12 -0500

    no se adelanto mucho

--- src/pages/RegisterUser.jsx
+++ src/pages/RegisterUser.jsx
@@ -4,7 +4,12 @@ import LOGO from "../images/LOGO.png";
 import { Button } from "antd";
 import InputPassword from "../shared/components/InputPassword";
 import InputLogin from "../shared/components/InputLogin";
+import Checkbox from "antd/es/checkbox/Checkbox";
+
 const RegisterUser = () => {
+  const onChange = (e) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
   return (
     <div className="register">
       <header>
@@ -16,6 +21,10 @@ const RegisterUser = () => {
         </div>
         <div className="content_left">
           <Register inputRegister={"Country"} />
+
+          <Checkbox onChange={onChange}>comprador</Checkbox>
+          <Checkbox onChange={onChange}>vendedor</Checkbox>
+          <Checkbox onChange={onChange}>ambos</Checkbox>
 
           <Register inputRegister={"Names"} />
           <Register inputRegister={"Last name"} />
