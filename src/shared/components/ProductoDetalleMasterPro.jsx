6d08eb93fff1f6f26c91d31c90ff8f4ddc93f607
juano2123 <juan_man.perlaza@uao.edu.co>
2023-03-02 23:24:44 -0500

    pagina de compra mela caramela 1A papa

--- src/shared/components/ProductoDetalleMasterPro.jsx
+++ src/shared/components/ProductoDetalleMasterPro.jsx
@@ -1,10 +1,33 @@
 import React from "react";
-
+//TERMINAR
 const ProductoDetalleMasterPro = () => {
   return (
     <div className="nose_todo">
       <div className="imageProduct">
         <img src="#" alt="" width={400} height={300} />
+      </div>
+
+      <div className="info">
+        <div className="infodetail">
+          <span>info detail</span>
+        </div>
+        <div className="price">
+          <span>precio al por mayor</span>
+        </div>
+        <div className="details_especific">
+          <div className="detail">
+            <span>color</span>
+          </div>
+          <div className="varition">
+            <ul>varicion de tipos de cosas</ul>
+          </div>
+        </div>
+        <div className="a_box_inner">
+          <span>The minimum order quantity is 2 piece 2/2 piece</span>
+          <span>from 6.50</span>
+          <span>shiping</span>
+          {/* la verdad son muchos div y me confundo TERMINAR */}
+        </div>
       </div>
     </div>
   );
