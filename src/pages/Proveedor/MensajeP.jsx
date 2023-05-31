608b92d397bce560aafb99b115c3b313c83b9cf4
tatiespi <tatiana.espinosa@uao.edu.co>
2023-05-31 08:26:46 -0500

    navegacion

--- src/pages/Proveedor/MensajeP.jsx
+++ src/pages/Proveedor/MensajeP.jsx
@@ -1,9 +1,122 @@
-import React from "react";
+import React, {useState} from "react";
 import MenuBarR from "../../shared/components/MenuBar";
 import { Radio, Upload, Button, Input } from "antd";
 import { SendOutlined, SearchOutlined, VideoCameraOutlined, FileOutlined, PictureOutlined, CloseOutlined, ShareAltOutlined, StarOutlined } from "@ant-design/icons";
+import "../styles/MensajeCliente.css";
 
 const MensajeP = () => {
+
+  const [chatSeleccionado, setChatSeleccionado] = useState(null);
+  const handleChatSeleccionado = (chatId) => {
+    setChatSeleccionado(chatId);
+  };
+
+  const renderMensaje = () => {
+    if (chatSeleccionado === 1) {
+      return (
+        <div className="mensaje">
+          <div className="burbuja-chat saliente">
+            <div className="avatar">
+              <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+            </div>
+            <div className="cuerpo">
+              <div className="texto">
+                <span>Contenido del primer mensaje saliente</span>
+              </div>
+              <span className="tiempo">
+                <i className="far fa-clock"></i>
+                Hace 5 min
+              </span>
+            </div>
+          </div>
+          <div className="burbuja-chat entrante">
+            <div className="avatar">
+              <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+            </div>
+            <div className="cuerpo">
+              <div className="texto">
+                <span>Contenido del primer mensaje entrante</span>
+              </div>
+              <span className="tiempo">
+                <i className="far fa-clock"></i>
+                Hace 5 min
+              </span>
+            </div>
+          </div>
+          {/* Otras burbujas de chat del primer mensaje */}
+        </div>
+      );
+    } else if (chatSeleccionado === 2) {
+      return (
+        <div className="mensaje">
+          <div className="burbuja-chat saliente">
+            <div className="avatar">
+              <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+            </div>
+            <div className="cuerpo">
+              <div className="texto">
+                <span>Contenido del segundo mensaje saliente</span>
+              </div>
+              <span className="tiempo">
+                <i className="far fa-clock"></i>
+                Hace 5 min
+              </span>
+            </div>
+          </div>
+          <div className="burbuja-chat entrante">
+            <div className="avatar">
+              <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+            </div>
+            <div className="cuerpo">
+              <div className="texto">
+                <span>Contenido del segundo mensaje entrante</span>
+              </div>
+              <span className="tiempo">
+                <i className="far fa-clock"></i>
+                Hace 5 min
+              </span>
+            </div>
+          </div>
+          {/* Otras burbujas de chat del segundo mensaje */}
+        </div>
+      );
+    } else if (chatSeleccionado === 3){
+      return (
+        <div className="mensaje">
+        <div className="burbuja-chat saliente">
+          <div className="avatar">
+            <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+          </div>
+          <div className="cuerpo">
+            <div className="texto">
+              <span>Contenido del tercer mensaje saliente</span>
+            </div>
+            <span className="tiempo">
+              <i className="far fa-clock"></i>
+              Hace 5 min
+            </span>
+          </div>
+        </div>
+        <div className="burbuja-chat entrante">
+          <div className="avatar">
+            <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+          </div>
+          <div className="cuerpo">
+            <div className="texto">
+              <span>Contenido del tercer mensaje entrante</span>
+            </div>
+            <span className="tiempo">
+              <i className="far fa-clock"></i>
+              Hace 5 min
+            </span>
+          </div>
+        </div>
+        {/* Otras burbujas de chat del segundo mensaje */}
+      </div>
+      );
+    }
+  };
+
   return (
     <div>
       <div className="menu-bar">
@@ -23,7 +136,7 @@ const MensajeP = () => {
             </div>
           </div>
           <div className="seccion-lista-usuarios">
-            <div className="usuario">
+            <div className="usuario" onClick={() =>handleChatSeleccionado(1)}>
               <div className="avatar">
               <img src="https://img.freepik.com/foto-gratis/retrato-hermoso-mujer-joven-posicion-pared-gris_231208-10760.jpg?w=2000" alt="img" />
                 
@@ -33,9 +146,9 @@ const MensajeP = () => {
                 <span>Nombre apellido</span>
                 <span>detalles de mensaje</span>
               </div>
-              <span className="notificacion">3</span>
+              {/* <span className="notificacion">3</span> */}
             </div>
-            <div className="usuario">
+            <div className="usuario" onClick={() =>handleChatSeleccionado(2)}>
               <div className="avatar">
                 <img src="https://www.shutterstock.com/image-photo/young-handsome-man-beard-wearing-260nw-1685438938.jpg" alt="img" />
                 <span className="estado-usuario ocupado"></span>
@@ -44,9 +157,9 @@ const MensajeP = () => {
                 <span>Nombre apellido</span>
                 <span>detalles de mensaje</span>
               </div>
-              <span className="notificacion">1</span>
+              <span className="notificacion"></span>
             </div>
-            <div className="usuario">
+            <div className="usuario" onClick={() =>handleChatSeleccionado(3)}>
               <div className="avatar">
                 <img src="https://www.shutterstock.com/image-photo/portrait-young-smiling-caucasian-man-260nw-1491969899.jpg" alt="img" />
                 <span className="estado-usuario desconectado"></span>
@@ -55,7 +168,7 @@ const MensajeP = () => {
                 <span>Nombre apellido</span>
                 <span>detalles de mensaje</span>
               </div>
-              <span className="notificacion">1</span>
+              {/* <span className="notificacion">1</span> */}
             </div>
           </div>
         </div>
@@ -80,9 +193,11 @@ const MensajeP = () => {
             </div>
           </div>
           <div className="panel-chat">
-            <div className="mensaje">
+          {renderMensaje()}
+            
+        <div>
               <div className="avatar">
-                <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+               
               </div>
               <div className="cuerpo">
                 {/* <img src="http://localhost/multimedia/png/user-foto-3.png" alt=""> */}
@@ -90,41 +205,41 @@ const MensajeP = () => {
   
                   <span className="tiempo">
                     <i className="far fa-clock"></i>
-                    Hace 5 min
+                   
                   </span>
                 </div>
                 <ul className="opciones-msj">
                   <li>
-                    <Button type="button" icon={<CloseOutlined />} />
+                    
                   </li>
                   <li>
-                    <Button type="button" icon={<ShareAltOutlined />} />
+                    
                   </li>
                 </ul>
               </div>
             </div>
             {/* derecha */}
-            <div className="mensaje left">
+            <div >
               <div className="cuerpo">
                 {/* <img src="http://localhost/multimedia/png/user-foto-3.png" alt=""> */}
                 <div className="texto">
                   
                   <span className="tiempo">
                     <i className="far fa-clock"></i>
-                    Hace 6 min
+                   
                   </span>
                 </div>
                 <ul className="opciones-msj">
                   <li>
-                    <Button type="button" icon={<CloseOutlined />} />
+                
                   </li>
                   <li>
-                    <Button type="button" icon={<ShareAltOutlined />} />
+                  
                   </li>
                 </ul>
               </div>
               <div className="avatar">
-                <img src="https://img.freepik.com/foto-gratis/feliz-joven_1098-20869.jpg" alt="img" />
+               
               </div>
             </div>
           </div>
